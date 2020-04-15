56a645df18222a35e9e07d217da72f80501914b3
Keita Aoki <akkt222@gmail.com>
2020-04-15 15:42:20 +0900

    Groups#newの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'twitodo#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  resources :groups, only: [:new]
 end
