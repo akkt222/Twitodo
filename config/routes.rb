a53e7a46ff747e671b70d45e1efc19edb66b2d1e
Keita Aoki <akkt222@gmail.com>
2020-04-18 19:49:24 +0900

    groupsのリソース

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   root 'twitodo#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-<<<<<<< HEAD
-  resources :groups, only: [:new]
-=======
+  resources :groups
   resources :tweets
   get "/tweets/search" => 'tweets#search'
->>>>>>> master
 end
