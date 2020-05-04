561e37d5599dbe99f6515f7fa9fe2cb11c65e778
Keita Aoki <akkt222@gmail.com>
2020-05-04 18:52:17 +0900

    ユーザー退会機能の実装

--- app/controllers/twitodo_controller.rb
+++ app/controllers/twitodo_controller.rb
@@ -1,4 +1,5 @@
 class TwitodoController < ApplicationController
+  # before_action :set_user
 
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'twitodo#index'
+  resources :twitodo, only: [:index, :show]
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   resources :groups
   get '/tweets', to: 'tweets#update'
-  # resources :tweets, only: [:update]
+  get '/user', to: 'users#destroy'
   get "/tweets/search" => 'tweets#search'
 end
